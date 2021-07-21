c46937257680b0403c50db8bd418469fa4f2e072
JOHANES CHRISTIAN LEWI PUTRAEL TARIGAN <johanes.christian@ui.ac.id>
2021-07-21 20:27:12 +0700

    Added page_not_found handler

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = create_app()
 # Handler if user attempts to access non-existing sites
 @app.errorhandler(404)
 def page_not_found(e):
-    return redirect("/")
+    return redirect("/"), 404
 
 
 if __name__ == "__main__":
